48468753faeded565c250dca2d105efe5f23fa62
Amanda Southworth <amanda@withastra.com>
2023-04-20 17:19:44 -0700

    More file set up and integration of library structure

--- senre/lib/senre.dart
+++ senre/lib/senre.dart
@@ -1,6 +1,7 @@
 part of senre;
 
-/// [SenreAnalytics] is the main encapsulation to interact with the 
-/// entire analytics options. 
+/// [SenreAnalytics] is the main encapsulation to interact with the
+/// entire analytics options. It's also where someone sets all of their
+/// variables to interact with Senre.
 
 class SenreAnalytics {}
--- senre/lib/senre_lib.dart
+++ senre/lib/senre_lib.dart
@@ -1,1 +1,11 @@
 library senre;
+
+// ----- COMPONENTS
+part 'Components/API Interactor/apiInteractor.dart';
+part 'Components/Event Tracking/eventTracking.dart';
+part 'Components/Experience Tracking/experienceTracking.dart';
+
+// ----- MODULES
+
+part 'Modules/Core Tool/coreToolAnalytics.dart';
+part 'Modules/Safety Plan/safetyPlanAnalytics.dart';
